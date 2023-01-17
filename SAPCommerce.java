aff6a5f09c903218e883687935789286597e0968
Carlos-Oliveira021 <ceovalone@hotmail.com>
2023-01-17 16:32:03 -0300

    Adiciona método para atualizar o carrinho

--- SAPCommerce.java
+++ SAPCommerce.java
@@ -1,8 +1,14 @@
 public class SAPCommerce {
+    
     public void addToCart(){
         System.out.println("Adicionando item ao carrinho");
     }
+    
     public void remmoveItemFromCart(){
         System.out.println("Removendo item ao carrinho");
+    }
+
+    public void updateItemFromCart(){
+        System.out.println("Atualiza item do carrinho");   
     }
 }
